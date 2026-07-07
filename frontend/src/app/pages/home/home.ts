0fb59945b7f3406cf8b33f2e0a0852edf9381b1e
Miguel Auad <migueauad@icloud.com>
2026-07-07 02:07:49 -0400

    mejoras en funcionalidades repechaje

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -50,11 +50,25 @@ export class HomeComponent implements OnInit {
       rolesPermitidos: ['ADMINISTRADOR','GERENTE TALLER'] //ADMINISTRADOR Y DUEÑO
     },
     {
+      titulo: 'Dinero Retenido',
+      descripcion: 'Consulta y simulación de fondos retenidos por emergencia.',
+      icono: 'M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z',
+      ruta: '/dinero-retenido',
+      rolesPermitidos: ['ADMINISTRADOR', 'CLIENTE']
+    },
+    {
       titulo: 'KPIs Operacionales',
       descripcion: 'Indicadores globales de emergencias, tiempos, SLA y eficiencia de talleres.',
       icono: 'M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z',
       ruta: '/kpis',
       rolesPermitidos: ['ADMINISTRADOR']
+    },
+    {
+      titulo: 'Asistente ChatBot IA',
+      descripcion: 'Triaje técnico automatizado para evaluación preliminar de fallas.',
+      icono: 'M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z',
+      ruta: '/triaje-chat',
+      rolesPermitidos: ['ADMINISTRADOR', 'GERENTE TALLER', 'MECANICO', 'CLIENTE'] //TODOS LOS ROLES
     }
   ];
 
